18e7cd5678d7ae32f501674cdb08048f2085e04d
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-09-13 22:16:26 -0400

    Add link to header title to route home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <nav className="header-container">
       <div className="title-container">
-        <h1>NYTimes Reader</h1>
+        <Link to="/" className="link-style">
+          <h1>NYTimes Reader</h1>
+        </Link>
       </div>
     </nav>
   );
