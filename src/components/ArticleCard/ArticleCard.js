e8e7265759d9c49195dbbf46c8210db84d96b0bf
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-09-14 17:37:03 -0400

    Remove abstract from articlecard

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -6,7 +6,6 @@ const ArticleCard = ({ title, byline, abstract }) => {
     <div className="article-card-container">
       <h3>{title}</h3>
       <h4>{byline}</h4>
-      <p>{abstract}</p>
     </div>
   );
 };
